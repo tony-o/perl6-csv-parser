31cfa5521e955ce74d9f602a5073a1ccab53fda5
Nick Logan <ugexe@cpan.org>
2015-11-14 14:59:28 -0500

    Make buf/str operations/checks less redundant
    This appears to give a slight speed improvment for running `t/`,
    but has room for further improvements by using less method calls
    (maybe cache the new generic method names instead of looking at
    $!binary each time). It should also allow easier optimizations
    to be made as there is only 1 code path in many spots where there
    was 2.

--- lib/CSV/Parser.pm
+++ lib/CSV/Parser.pm
@@ -4,100 +4,87 @@ class CSV::Parser {
   has Bool       $.binary              = False;
   has IO::Handle $.file_handle         = Nil;
   has Bool       $.contains_header_row = False;
-  has Any        $.field_separator     = ',';
-  has Any        $.line_separator      = "\n";
-  has Any        $.field_operator      = '"';
-  has Any        $.escape_operator     = '\\';
-  has Int        $.chunk_size          = 1024;
-  has Int        $!fpos                = 0;
-  has Int        $!bpos                = 0;
-  has Int        $!bopn                = 0;
-  has Any        $!lbuff               = '';
+  has            $.field_separator     = ',';
+  has            $.line_separator      = "\n";
+  has            $.field_operator      = '"';
+  has            $.escape_operator     = '\\';
+  has int        $.chunk_size          = 1024;
+  has int        $!fpos                = 0;
+  has int        $!bpos                = 0;
+  has int        $!bopn                = 0;
+  has            $!lbuff               = '';
 
-  has Any        %!headers;
+  has        %!headers;
 
   method reset () {
-    my $p = $.file_handle.path;
-    $.file_handle.close;
-    $.file_handle = open $p, :r;
+    my $p = $!file_handle.path;
+    $!file_handle.close;
+    $!file_handle = open $p, :r;
   }
 
   method get_line () {
-    return Nil if $.file_handle.eof;
-    $!lbuff = $!lbuff == '' ?? ( $.binary == True ?? Buf.new() !! '' ) !! $!lbuff;
-    my Any $buffer = $!lbuff;
+    return Nil if $!file_handle.eof;
+    $!lbuff = Buf.new() if ?$!binary && not "{$!lbuff}";
+    my $buffer = $!lbuff;
+    $!bpos = $!bopn = 0;
 
-    $!bpos = 0;
-    $!bopn = 0;
-    while my Any $line = ( $.binary == True ?? $.file_handle.read($.chunk_size) !! $.file_handle.get ) {
-      $buffer = $buffer ~ $line;
-      $buffer ~= "\n" if ( $.binary == False );
-      $buffer ~= $.line_separator if ( $.binary == True );
-      last if $.detect_end_line( $buffer ) == 1;
+    while ( ?$!binary ?? $!file_handle.read($!chunk_size) !! $!file_handle.get ) -> $line {
+      $buffer ~= $line ~ $!line_separator;
+      last if self.detect_end_line( $buffer ) == 1;
     }
-    if ($.binary == True) {
-      $buffer = $buffer.subbuf(0, $buffer.bytes - $.line_separator.bytes);
-      $!lbuff = $buffer.subbuf($!bpos - 1);
-      $buffer = $buffer.subbuf(0, $!bpos);
-    } else {
-      $buffer = $buffer.substr(0, $buffer.chars - 1);
-      $!lbuff = $buffer.substr($!bpos - 1);
-      $buffer = $buffer.substr(0, $!bpos);
+
+    if ($buffer."{self!sizer}"() - $!line_separator."{self!sizer}"()) -> $size {
+      $buffer  = $buffer."{self!subber}"(0, $size);
+      $!lbuff  = $buffer."{self!subber}"($!bpos - 1);
+      $buffer  = $buffer."{self!subber}"(0, $!bpos);
     }
-    if ( $!contains_header_row ) { 
-      %!headers = %($.parse( $buffer ));
+
+    !$!contains_header_row ?? %(self.parse( $buffer )) !! do {
+      %!headers = %(self.parse( $buffer ));
       $!contains_header_row = False;
-      return $.get_line();
+      self.get_line();
     }
-    return %($.parse( $buffer ));
   };
 
+  method !sizer  { ?$!binary ?? "bytes"  !! "chars"  }
+  method !subber { ?$!binary ?? "subbuf" !! "substr" }
+  method !cmper($a,$b)  { ?$!binary ?? ($a eqv $b) !! ($a eq $b) } # make me an infix
+
   method parse ( $line ) returns Hash {
-    my Any %values    = ();
-    my Any %header    = %!headers;
-    my Int $fcnt      = 0;
-    my Any $localbuff = $.binary == True ?? Buf.new() !! '';
-    my Int $buffpos   = 0;
-    my Any $buffer    = $line;
-    my Int $bopn      = 0;
-    my Any $key;
+    my %values      = ();
+    my %header      = %!headers;
+    my $localbuff   = ?$!binary ?? Buf.new() !! '';
+    my $buffer      = $line;
+    my int $fcnt    = 0;
+    my int $buffpos = 0;
+    my int $bopn    = 0;
+    my $key;
     #my $reg       = /^{$.field_operator}|{$.field_operator}$/; #this shit isn't implemented yet
 
-    while ($.binary == False && $buffpos < $buffer.chars) || ($.binary == True && $buffpos < $buffer.bytes) {
-      if ( ( ( $.binary == False && $buffer.substr($buffpos, $.field_operator.chars) eq  $.field_operator ) || 
-             ( $.binary == True && $buffer.subbuf($buffpos, $.field_operator.bytes) eqv $.field_operator ) ) &&
-           ( ( $.binary == False && $localbuff ne   $.escape_operator ) || 
-             ( $.binary == True && $localbuff !eqv $.escape_operator ) ) ) {
+    while $buffpos < $buffer."{self!sizer}"() {
+      if self!cmper($buffer."{self!subber}"($buffpos, $!field_operator."{self!sizer}"()), $!field_operator )
+            && !self!cmper($localbuff, $!escape_operator) {
         $bopn = $bopn == 1 ?? 0 !! 1;
       }
-      if ( ( ( $.binary == False && $buffer.substr($buffpos, $.field_separator.chars) eq  $.field_separator ) ||
-             ( $.binary == True && $buffer.subbuf($buffpos, $.field_separator.bytes) eqv $.field_separator ) ) &&
-           ( ( $.binary == False && $localbuff ne   $.escape_operator ) || 
-             ( $.binary == True && $localbuff !eqv $.escape_operator ) ) &&
-           $bopn == 0 ) {
+
+      if self!cmper($buffer."{self!subber}"($buffpos, $!field_separator."{self!sizer}"()), $!field_separator )
+            && !self!cmper($localbuff, $!escape_operator) && $bopn == 0 {
         $key = %header{(~$fcnt)}:exists ?? %header{~$fcnt} !! $fcnt;
-        if ($.binary == True) {
-          %values{ $key } = $buffer.subbuf(0, $buffpos);
-          %values{ $key } = %values{ $key }.subbuf($.field_operator.bytes, %values{ $key }.bytes - ( $.field_operator.bytes * 2 )) if %values{ $key }.subbuf(0, $.field_operator.bytes) eqv $.field_operator;
-          $buffer = $buffer.subbuf($buffpos+$.field_separator.bytes);
-        } else {
-          %values{ $key } = $buffer.substr(0, $buffpos);
-          %values{ $key } = %values{ $key }.substr($.field_operator.chars, %values{ $key }.chars - ( $.field_operator.chars * 2 )) if %values{ $key }.substr(0, $.field_operator.chars) eq  $.field_operator;
-          $buffer = $buffer.substr($buffpos+$.field_separator.chars);
-        }
+        %values{ $key } = $buffer."{self!subber}"(0, $buffpos);
+        %values{ $key } = %values{ $key }."{self!subber}"($!field_operator."{self!sizer}"(), %values{ $key }."{self!sizer}"() - ( $!field_operator."{self!sizer}"() * 2 ))\
+          if self!cmper( %values{ $key }."{self!subber}"(0, $!field_operator."{self!sizer}"()), $!field_operator);
+        $buffer = $buffer."{self!subber}"($buffpos+$!field_separator."{self!sizer}"());
         $buffpos = 0;
         $fcnt++;
         next;
       }
       
-      $localbuff = ($localbuff.chars >= $.escape_operator.chars ?? $localbuff.substr(1) !! $localbuff) ~ $buffer.substr($buffpos, 1) if $.binary == False;
-      $localbuff = ($localbuff.bytes >= $.escape_operator.bytes ?? $localbuff.subbuf(1) !! $localbuff) ~ $buffer.subbuf($buffpos, 1) if $.binary == True; 
+      $localbuff = ($localbuff."{self!sizer}"() >= $!escape_operator."{self!sizer}"() ?? $localbuff."{self!subber}"(1) !! $localbuff) ~ $buffer."{self!subber}"($buffpos, 1);
       $buffpos++;
     }
     $key = %header{~$fcnt}:exists ?? %header{~$fcnt} !! $fcnt;
     %values{ $key } = $buffer;
-    %values{ $key } = %values{ $key }.substr($.field_operator.chars, %values{ $key }.chars - ( $.field_operator.chars * 2 )) if $.binary == False && %values{ $key }.substr(0, $.field_operator.chars) eq  $.field_operator;
-    %values{ $key } = %values{ $key }.subbuf($.field_operator.bytes, %values{ $key }.bytes - ( $.field_operator.bytes * 2 )) if $.binary == True && %values{ $key }.subbuf(0, $.field_operator.bytes) eqv $.field_operator;
+    %values{ $key } = %values{ $key }."{self!subber}"($!field_operator."{self!sizer}"(), %values{ $key }."{self!sizer}"() - ( $!field_operator."{self!sizer}"() * 2 )) if self!cmper( %values{ $key }."{self!subber}"(0, $!field_operator."{self!sizer}"()), $!field_operator);
 
     while %header{~(++$fcnt)}:exists {
       %values{%header{~$fcnt}} = Nil;
@@ -106,29 +93,23 @@ class CSV::Parser {
     return %values;
   };
 
-  method detect_end_line ( $buffer ) returns Int {
-    my Any $localbuff = $.binary == True ?? Buf.new !! '';
-    while $!bpos < ( $.binary == True ?? $buffer.bytes !! $buffer.chars ) {
-      if ( ( ( $.binary == False && $buffer.substr($!bpos, $.field_operator.chars) eq  $.field_operator ) || 
-             ( $.binary == True && $buffer.subbuf($!bpos, $.field_operator.bytes) eqv $.field_operator ) ) &&
-           ( ( $.binary == False && $localbuff ne   $.escape_operator ) || 
-             ( $.binary == True && $localbuff !eqv $.escape_operator ) ) ) {
+  method detect_end_line ( $buffer ) returns Bool {
+    my $localbuff = ?$!binary ?? Buf.new() !! '';
+    while $!bpos < $buffer."{self!sizer}"() {
+      if self!cmper($buffer."{self!subber}"($!bpos, $!field_operator."{self!sizer}"()), $!field_operator )
+            && !self!cmper($localbuff, $!escape_operator) {
         $!bopn = $!bopn == 1 ?? 0 !! 1;
       }
 
       #detect line separator
-      if ( ( ( $.binary == False && $buffer.substr($!bpos, $.line_separator.chars) eq  $.line_separator ) ||
-             ( $.binary == True && $buffer.subbuf($!bpos, $.line_separator.bytes) eqv $.line_separator ) ) && 
-           ( ( $.binary == False && $localbuff ne   $.escape_operator ) ||
-             ( $.binary == True && $localbuff !eqv $.escape_operator ) ) && 
-           $!bopn == 0 ) {
+      if self!cmper($buffer."{self!subber}"($!bpos, $!line_separator."{self!sizer}"()), $!line_separator )
+            && !self!cmper($localbuff, $!escape_operator) && $!bopn == 0 {
         $!bpos++;
-        return 1;
+        return True;
       }
-      $localbuff = ($localbuff.chars >= $.escape_operator.chars ?? $localbuff.substr(1) !! $localbuff) ~ $buffer.substr($!bpos, 1) if $.binary == False;
-      $localbuff = ($localbuff.bytes >= $.escape_operator.bytes ?? $localbuff.subbuf(1) !! $localbuff) ~ $buffer.subbuf($!bpos, 1) if $.binary == True;
+      $localbuff = ($localbuff."{self!sizer}"() >= $!escape_operator."{self!sizer}"() ?? $localbuff."{self!subber}"(1) !! $localbuff) ~ $buffer."{self!subber}"($!bpos, 1);
       $!bpos++;
     }
-    return 0;
+    return False;
   };
 };
